2f3c90c8064859d2d6607ed1e1760361c41c5b66
Vijaya Sakri <vijayasakri7@gmail.com>
2025-02-26 20:35:48 +0530

    modified allowed hosts

--- SponsorsMedia/settings.py
+++ SponsorsMedia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost"]
-ALLOWED_HOSTS +=os.environ.get("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "vijaya24.pythonanywhere.com").split()
 
 # Application definition
 
